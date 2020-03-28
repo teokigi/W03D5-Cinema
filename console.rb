8f2d8bd2912a5d5aad837887c95d7b8a7bbf0861
Tony Tang <teokigi@hotmail.com>
2020-03-28 19:18:40 +0000

    added req_rel screening to console

--- console.rb
+++ console.rb
@@ -2,6 +2,7 @@ require('pry-byebug')
 require_relative('models/ticket.rb')
 require_relative('models/film.rb')
 require_relative('models/customer.rb')
+require_relative('models/screening.rb')
 
 loop_program = "1"
 while loop_program == "1"
