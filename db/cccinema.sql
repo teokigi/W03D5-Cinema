9d9fd8fef0cdea874d309dbc64662fa4a1502d62
Tony Tang <teokigi@hotmail.com>
2020-03-28 18:55:29 +0000

    recoded sql

--- db/cccinema.sql
+++ db/cccinema.sql
@@ -1,33 +1,32 @@
-    #drop if existing tables
+    --drop if existing tables
 DROP TABLE IF EXISTS tickets;
-DROP TABLE IF EXISTS films;
 DROP TABLE IF EXISTS screenings;
+DROP TABLE IF EXISTS films;
 DROP TABLE IF EXISTS customers;
-    #create Customers table.
+    --create Customers table.
 CREATE TABLE customers
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     funds INT
 );
-    # create a screenings table.
-CREATE TABLE screenings
-(
-    id SERIAL PRIMARY KEY,
-    show_time VARCHAR(255),
-    available_tickets INT,
-    sold_tickets INT,
-    total_tickets AS available_tickets + sold_tickets PERSISTED
-);
-    #create a films table.
+    --create a films table.
 CREATE TABLE films
 (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    screening_id REFERENCES screenings(id) ON DELETE CASCADE,
     price INT
 );
-    #create a tickets table.
+--create a screenings table.
+CREATE TABLE screenings
+(
+id SERIAL PRIMARY KEY,
+film_id INT REFERENCES films(id) ON DELETE CASCADE,
+show_time VARCHAR(255),
+availability INT NOT NULL,
+sales INT DEFAULT 0
+);
+    --create a tickets table.
 CREATE TABLE tickets
 (
     id SERIAL PRIMARY KEY,
