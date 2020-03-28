ff0266dd664f4008c98328d5552d35d1a3a44200
Tony Tang <teokigi@hotmail.com>
2020-03-28 19:17:38 +0000

    changed data and added screening data

--- console.rb
+++ console.rb
@@ -48,46 +48,107 @@ while loop_program == "1"
             film5 = Film.new({'title'=>'Elfen Lied','price'=>14})
             film5.save
 
+            screening01 = Screening.new({
+                                            'film_id'=>film1.id,
+                                            'showtime'=>'1000',
+                                            'availability'=>2
+                                        })
+            screening01.save
+            screening02 = Screening.new({
+                                            'film_id'=>film1.id,
+                                            'showtime'=>'2200',
+                                            'availability'=>3
+                                        })
+            screening02.save
+            screening03 = Screening.new({
+                                            'film_id'=>film2.id,
+                                            'showtime'=>'1000',
+                                            'availability'=>3
+                                        })
+            screening03.save
+            screening04 = Screening.new({
+                                            'film_id'=>film2.id,
+                                            'showtime'=>'2200',
+                                            'availability'=>2
+                                        })
+            screening04.save
+            screening05 = Screening.new({
+                                            'film_id'=>film3.id,
+                                            'showtime'=>'1000',
+                                            'availability'=>2
+                                        })
+            screening05.save
+            screening06 = Screening.new({
+                                            'film_id'=>film3.id,
+                                            'showtime'=>'2200',
+                                            'availability'=>3
+                                        })
+            screening06.save
+            screening07 = Screening.new({
+                                            'film_id'=>film4.id,
+                                            'showtime'=>'1000',
+                                            'availability'=>3
+                                        })
+            screening07.save
+            screening08 = Screening.new({
+                                            'film_id'=>film4.id,
+                                            'showtime'=>'2200',
+                                            'availability'=>2
+                                        })
+            screening08.save
+            screening09 = Screening.new({
+                                            'film_id'=>film5.id,
+                                            'showtime'=>'1000',
+                                            'availability'=>2
+                                        })
+            screening09.save
+            screening10 = Screening.new({
+                                            'film_id'=>film5.id,
+                                            'showtime'=>'2200',
+                                            'availability'=>3
+                                        })
+            screening10.save
 
-            ticket01 = Ticket.new({'customer_id'=>customer01.id,'film_id'=>film1.id})
+
+            ticket01 = Ticket.new({'customer_id'=>customer01.id,'screening_id'=>screening1.id})
             ticket01.save
-            ticket02 = Ticket.new({'customer_id'=>customer01.id,'film_id'=>film2.id})
+            ticket02 = Ticket.new({'customer_id'=>customer01.id,'screening_id'=>screening2.id})
             ticket02.save
-            ticket03 = Ticket.new({'customer_id'=>customer02.id,'film_id'=>film1.id})
+            ticket03 = Ticket.new({'customer_id'=>customer02.id,'screening_id'=>screening3.id})
             ticket03.save
-            ticket04 = Ticket.new({'customer_id'=>customer02.id,'film_id'=>film2.id})
+            ticket04 = Ticket.new({'customer_id'=>customer02.id,'screening_id'=>screening1.id})
             ticket04.save
-            ticket05 = Ticket.new({'customer_id'=>customer03.id,'film_id'=>film3.id})
+            ticket05 = Ticket.new({'customer_id'=>customer03.id,'screening_id'=>screening2.id})
             ticket05.save
-            ticket06 = Ticket.new({'customer_id'=>customer03.id,'film_id'=>film1.id})
+            ticket06 = Ticket.new({'customer_id'=>customer03.id,'screening_id'=>screening3.id})
             ticket06.save
-            ticket07 = Ticket.new({'customer_id'=>customer04.id,'film_id'=>film2.id})
+            ticket07 = Ticket.new({'customer_id'=>customer04.id,'screening_id'=>screening4.id})
             ticket07.save
-            ticket08 = Ticket.new({'customer_id'=>customer04.id,'film_id'=>film3.id})
+            ticket08 = Ticket.new({'customer_id'=>customer04.id,'screening_id'=>screening1.id})
             ticket08.save
-            ticket09 = Ticket.new({'customer_id'=>customer05.id,'film_id'=>film4.id})
+            ticket09 = Ticket.new({'customer_id'=>customer05.id,'screening_id'=>screening2.id})
             ticket09.save
-            ticket10 = Ticket.new({'customer_id'=>customer05.id,'film_id'=>film1.id})
+            ticket10 = Ticket.new({'customer_id'=>customer05.id,'screening_id'=>screening3.id})
             ticket10.save
-            ticket11 = Ticket.new({'customer_id'=>customer06.id,'film_id'=>film2.id})
+            ticket11 = Ticket.new({'customer_id'=>customer06.id,'screening_id'=>screening4.id})
             ticket11.save
-            ticket12 = Ticket.new({'customer_id'=>customer06.id,'film_id'=>film3.id})
+            ticket12 = Ticket.new({'customer_id'=>customer06.id,'screening_id'=>screening5.id})
             ticket12.save
-            ticket13 = Ticket.new({'customer_id'=>customer07.id,'film_id'=>film4.id})
+            ticket13 = Ticket.new({'customer_id'=>customer07.id,'screening_id'=>screening1.id})
             ticket13.save
-            ticket14 = Ticket.new({'customer_id'=>customer07.id,'film_id'=>film5.id})
+            ticket14 = Ticket.new({'customer_id'=>customer07.id,'screening_id'=>screening2.id})
             ticket14.save
-            ticket15 = Ticket.new({'customer_id'=>customer08.id,'film_id'=>film1.id})
+            ticket15 = Ticket.new({'customer_id'=>customer08.id,'screening_id'=>screening3.id})
             ticket15.save
-            ticket16 = Ticket.new({'customer_id'=>customer08.id,'film_id'=>film2.id})
+            ticket16 = Ticket.new({'customer_id'=>customer08.id,'screening_id'=>screening4.id})
             ticket16.save
-            ticket17 = Ticket.new({'customer_id'=>customer09.id,'film_id'=>film3.id})
+            ticket17 = Ticket.new({'customer_id'=>customer09.id,'screening_id'=>screening5.id})
             ticket17.save
-            ticket18 = Ticket.new({'customer_id'=>customer09.id,'film_id'=>film4.id})
+            ticket18 = Ticket.new({'customer_id'=>customer09.id,'screening_id'=>screening6.id})
             ticket18.save
-            ticket19 = Ticket.new({'customer_id'=>customer10.id,'film_id'=>film5.id})
+            ticket19 = Ticket.new({'customer_id'=>customer10.id,'screening_id'=>screening1.id})
             ticket19.save
-            ticket20 = Ticket.new({'customer_id'=>customer10.id,'film_id'=>film1.id})
+            ticket20 = Ticket.new({'customer_id'=>customer10.id,'screening_id'=>screening2.id})
             ticket20.save
             p "table repopulated."
         when "3"
